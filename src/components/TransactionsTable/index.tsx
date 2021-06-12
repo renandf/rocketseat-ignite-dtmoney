9042d3867e8ab02771c7714a99b38dbfdd94c2c0
Renan Castro <renandf@gmail.com>
2021-06-12 18:25:17 +1000

    feat: Display data from Mirage DB into rows of TransactionsTable

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from '../../services/api';
 import { Container } from "./styles";
 
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
+
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
   useEffect(() => {
     api.get('transactions')
-      .then(response => console.log(response.data))
+      .then(response => setTransactions(response.data.transactions))
   }, [])
 
   return (
@@ -21,24 +32,14 @@ export function TransactionsTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Web development</td>
-            <td>$12,000</td>
-            <td>Development</td>
-            <td>20/02/2021</td>
-          </tr>
-          <tr>
-            <td>Web development</td>
-            <td className="deposit">$12,000</td>
-            <td>Development</td>
-            <td>20/02/2021</td>
-          </tr>
-          <tr>
-            <td>Web development</td>
-            <td className="withdraw">-$12,000</td>
-            <td>Development</td>
-            <td>20/02/2021</td>
-          </tr>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>{transaction.amount}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Website freelance',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Rent',
+          type: 'withdraw',
+          category: 'Housing',
+          amount: 2200,
+          createdAt: new Date('2021-03-01 11:00:00'),
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
